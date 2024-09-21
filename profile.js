0aecdbad8db195eda12fa53617eaf0c664412f41
zawminsoe <alinyaung225800@gmail.com>
2024-09-21 08:12:41 +0630

    add additional features

--- profile.js
+++ profile.js
@@ -2,6 +2,7 @@ let selector = (ele) => document.querySelector(ele);
 let allSelector = (ele) => document.querySelectorAll(ele);
 
 let lightBackgrounds = allSelector(".bg-light");
+let blueBackgrounds = allSelector(".bg-primary");
 let lightTexts = allSelector(".text-light");
 
 let themeToggler = selector(".themeToggleBtn");
@@ -10,9 +11,13 @@ themeToggler.addEventListener("click", () => {
   lightBackgrounds.forEach((lightBackground) => {
     lightBackground.classList.toggle("bg-dark");
   });
+  blueBackgrounds.forEach((blueBackground) => {
+    blueBackground.classList.toggle("bg-dark");
+  });
   lightTexts.forEach((lightText) => {
     lightText.classList.toggle("text-dark");
   });
+
   themeValue.textContent == "light_mode"
     ? (themeValue.textContent = "dark_mode")
     : (themeValue.textContent = "light_mode");
